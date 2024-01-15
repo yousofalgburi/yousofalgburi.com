5bf979799cfe152c4f806dc69f2d479e2d9a7dd0
Yousof Algburi <yousofalgburi@gmail.com>
2024-01-15 17:59:47 -0500

    fixing og settings v4

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -6,10 +6,6 @@ export const runtime = 'edge'
 export async function GET(req: NextRequest) {
 	const { searchParams } = req.nextUrl
 	const postTitle = searchParams.get('title')
-	const font = fetch(new URL('../../public/fonts/kaisei-tokumin-bold.ttf', import.meta.url)).then((res) =>
-		res.arrayBuffer()
-	)
-	const fontData = await font
 
 	return new ImageResponse(
 		(
@@ -17,41 +13,18 @@ export async function GET(req: NextRequest) {
 				style={{
 					height: '100%',
 					width: '100%',
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'flex-start',
-					justifyContent: 'center',
 					backgroundColor: '#111010',
 				}}
 			>
 				<div
 					style={{
-						marginLeft: 190,
-						marginRight: 190,
-						display: 'flex',
-						fontSize: 130,
-						fontFamily: 'Kaisei Tokumin',
-						letterSpacing: '-0.05em',
-						fontStyle: 'normal',
+						fontFamily: 'Arial, sans-serif',
 						color: 'white',
-						lineHeight: '120px',
-						whiteSpace: 'pre-wrap',
 					}}
 				>
 					{postTitle}
 				</div>
 			</div>
-		),
-		{
-			width: 1920,
-			height: 1080,
-			fonts: [
-				{
-					name: 'Kaisei Tokumin',
-					data: fontData,
-					style: 'normal',
-				},
-			],
-		}
+		)
 	)
 }
