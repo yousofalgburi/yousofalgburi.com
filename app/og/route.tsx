e16b87d230b0307f9da9e3353cb9887dba0a988e
Yousof Algburi <yousofalgburi@gmail.com>
2024-01-15 17:56:38 -0500

    fixing og settings v3

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -6,6 +6,10 @@ export const runtime = 'edge'
 export async function GET(req: NextRequest) {
 	const { searchParams } = req.nextUrl
 	const postTitle = searchParams.get('title')
+	const font = fetch(new URL('../../public/fonts/kaisei-tokumin-bold.ttf', import.meta.url)).then((res) =>
+		res.arrayBuffer()
+	)
+	const fontData = await font
 
 	return new ImageResponse(
 		(
@@ -14,19 +18,24 @@ export async function GET(req: NextRequest) {
 					height: '100%',
 					width: '100%',
 					display: 'flex',
-					alignItems: 'center',
+					flexDirection: 'column',
+					alignItems: 'flex-start',
 					justifyContent: 'center',
-					backgroundImage: 'url(https://yousofalgburi.com/og-bg.png)',
+					backgroundColor: '#111010',
 				}}
 			>
 				<div
 					style={{
-						textAlign: 'center',
-						fontSize: 100,
-						fontFamily: 'Arial, sans-serif',
+						marginLeft: 190,
+						marginRight: 190,
+						display: 'flex',
+						fontSize: 130,
+						fontFamily: 'Kaisei Tokumin',
+						letterSpacing: '-0.05em',
+						fontStyle: 'normal',
 						color: 'white',
-						lineHeight: '110px',
-						padding: '0 20px',
+						lineHeight: '120px',
+						whiteSpace: 'pre-wrap',
 					}}
 				>
 					{postTitle}
@@ -36,6 +45,13 @@ export async function GET(req: NextRequest) {
 		{
 			width: 1920,
 			height: 1080,
+			fonts: [
+				{
+					name: 'Kaisei Tokumin',
+					data: fontData,
+					style: 'normal',
+				},
+			],
 		}
 	)
 }
