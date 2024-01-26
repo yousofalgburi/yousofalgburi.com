5a9e00c63b54b10e26af4ab0d0b945611b03d154
Yousof Algburi <yousofalgburi@gmail.com>
2024-01-25 22:31:49 -0500

    update projects section

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,6 +9,19 @@ export default function BlogPage() {
 	return (
 		<section>
 			<h1 className='font-medium text-2xl mb-8 tracking-tighter'>Checkout my projects</h1>
+
+			<h3 className='text-lg tracking-tighter'>
+				<Link href='https://games-link.vercel.app/'>GamesLink</Link>
+			</h3>
+
+			<h2 className='text-xl my-8 tracking-tighter'>Older projects</h2>
+			<h3 className='text-lg tracking-tighter'>
+				<Link href='https://github.com/yousofalgburi/Bug-Tracker-PERN-Stack'>Bugs Tracker PERN</Link>
+			</h3>
+
+			<h3 className='text-lg tracking-tighter'>
+				<Link href='https://github.com/yousofalgburi/destination-social-app'>Destination Social MERN</Link>
+			</h3>
 		</section>
 	)
 }
