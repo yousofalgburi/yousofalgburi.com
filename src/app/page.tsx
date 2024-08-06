e877fd8ece7df54fe623db07c678182922f83fe0
Yousof Algburi <yousofalgburi@gmail.com>
2024-08-05 20:52:29 -0400

    fix bad link v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Page() {
 								<CardHeader>
 									<div className='flex items-center justify-between gap-x-2 text-base'>
 										<h3 className='inline-flex items-center justify-center gap-x-1 font-semibold leading-none'>
-											<a className='hover:underline' href={work.link}>
+											<a className='hover:underline' href={work.link ?? ''}>
 												{work.company}
 											</a>
 
